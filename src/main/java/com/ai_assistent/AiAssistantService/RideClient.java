623fb353df302cc048ed8178646d7e077d5ac7dc
Shivam Yadav <sy4555791@gmail.com>
2025-07-20 10:55:11 +0530

    Update RideClient.java

--- src/main/java/com/ai_assistent/AiAssistantService/RideClient.java
+++ src/main/java/com/ai_assistent/AiAssistantService/RideClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "RiderService", url = "http://localhost:8083")
+@FeignClient(name = "RiderService")
 public interface RideClient {
     @GetMapping("/ride/latest/{userId}")
     RideDto getLatestRide(@PathVariable("userId") String userId);
