4ae4672e7e77752b82a78511b5c8825030cc2403
Shivam Yadav <sy4555791@gmail.com>
2025-08-05 09:03:00 +0530

    Update DistanceService.java

--- src/main/java/com/rider_service/RiderService/service/DistanceService.java
+++ src/main/java/com/rider_service/RiderService/service/DistanceService.java
@@ -15,29 +15,7 @@ import java.util.Random;
 
 @Service
 public class DistanceService {
-
-//    @Value("${google.maps.key}")
-//    private String apiKey;
-//
     public double getDistanceInKm(String source, String destination) {
-//        String url = String.format("https://maps.googleapis.com/maps/api/distancematrix/json?origins=%s&destinations=%s&key=%s",
-//                UriUtils.encodePath(source , StandardCharsets.UTF_8),
-//                UriUtils.encodePath(destination , StandardCharsets.UTF_8),
-//                apiKey
-//                );
-//
-//        RestTemplate restTemplate = new RestTemplate();
-//        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-//
-//        org.json.JSONObject json = new org.json.JSONObject(response.getBody());
-//
-//        JSONArray rows = json.getJSONArray("rows");
-//        org.json.JSONObject elements = new org.json.JSONObject(0).getJSONArray("elements").getJSONObject(0);
-//        int distanceInMeters = elements.getJSONObject("distance").getInt("value");
-//
-//        return distanceInMeters/1000.0;
-//    }
-
     if (source.equalsIgnoreCase(destination)) {
         return 1.0;
     }
@@ -46,8 +24,8 @@ public class DistanceService {
         return 8.5;
     }
 
-        return 15.0 + new Random().nextInt(10);
-
+        return 15.0 + new Random().nextInt(10){
 
 }
     }
+
